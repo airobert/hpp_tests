dc7e108d176f593774de282ff37599e11a388446
Robert White <ai.robert.wangshuai@gmail.com>
2016-06-03 11:41:48 +0200

    ROS

---
+++ rosworkspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/swang/Project/hpp_test/rosworkspace/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/swang/Project/HPP/install;/opt/ros/indigo".split(';') if "/home/swang/Project/HPP/install;/opt/ros/indigo" != "" else []
---
+++ rosworkspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/swang/Project/hpp_test/rosworkspace/devel:$CMAKE_PREFIX_PATH"
+export PATH="/home/swang/Project/HPP/install/bin:/opt/ros/indigo/bin:/home/swang/Project/HPP/install/sbin:/home/swang/Project/HPP/install/sbin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/cuda-7.5/bin"
+export PWD="/home/swang/Project/hpp_test/rosworkspace/build"
+export PYTHONPATH="/opt/ros/indigo/lib/python2.7/dist-packages:/home/swang/Project/HPP/install/lib/python2.7/site-packages:/home/swang/Project/HPP/install/lib/python2.7/site-packages"
+export ROS_PACKAGE_PATH="/home/swang/Project/hpp_test/rosworkspace/src:$ROS_PACKAGE_PATH"
