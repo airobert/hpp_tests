4b59195ce3797de7947a68529b043785a0ebb214
Robert White <ai.robert.wangshuai@gmail.com>
2016-06-20 16:11:53 +0200

    offset were changed and now it's walking like drunk

--- rbprm_tests/simple_boeing_contact.py
+++ rbprm_tests/simple_boeing_contact.py
@@ -22,7 +22,7 @@ fullBody = FullBody ()
 fullBody.loadFullBodyModel(urdfName, rootJointType, meshPackageName, packageName, urdfSuffix, srdfSuffix)
 
 #  Setting a number of sample configurations used
-nbSamples = 20000
+nbSamples = 10000
 
 ps = tp.ProblemSolver(fullBody)
 r = tp.Viewer (ps)
@@ -50,7 +50,7 @@ fullBody.addLimb(rLegId,rLeg,rfoot,rLegOffset,rLegNormal, rLegx, rLegy, nbSample
 lLegId = 'lhleg'
 lLeg = 'lh_haa_joint'
 lfoot = 'lh_foot_joint'
-lLegOffset = [0,0,0]
+lLegOffset = [0.,-0.021,0.]
 lLegNormal = [0,1,0]
 lLegx = 0.02; lLegy = 0.02
 fullBody.addLimb(lLegId,lLeg,lfoot,lLegOffset,rLegNormal, lLegx, lLegy, nbSamples, "manipulability", 0.05, cType)
@@ -58,7 +58,7 @@ fullBody.addLimb(lLegId,lLeg,lfoot,lLegOffset,rLegNormal, lLegx, lLegy, nbSample
 rarmId = 'rhleg'
 rarm = 'rh_haa_joint'
 rHand = 'rh_foot_joint'
-rArmOffset = [0.,0,-0.]
+rArmOffset = [0.,-0.021,0.]
 rArmNormal = [0,1,0]
 rArmx = 0.02; rArmy = 0.02
 fullBody.addLimb(rarmId,rarm,rHand,rArmOffset,rArmNormal, rArmx, rArmy, nbSamples, "manipulability", 0.05, cType)
@@ -66,15 +66,21 @@ fullBody.addLimb(rarmId,rarm,rHand,rArmOffset,rArmNormal, rArmx, rArmy, nbSample
 larmId = 'lfleg'
 larm = 'lf_haa_joint'
 lHand = 'lf_foot_joint'
-lArmOffset = [0.,0,-0.]
+lArmOffset = [0.,-0.021,0.]
 lArmNormal = [0,1,0]
 lArmx = 0.02; lArmy = 0.02
 fullBody.addLimb(larmId,larm,lHand,lArmOffset,lArmNormal, lArmx, lArmy, nbSamples, "forward", 0.05, cType)
 # RB: this line is never used?
 q_0 = fullBody.getCurrentConfig(); 
 
+fullBody.client.basic.robot.setJointConfig('lf_hfe_joint',[-1.4])
+fullBody.client.basic.robot.setJointConfig('lh_hfe_joint',[-1.4])
+fullBody.client.basic.robot.setJointConfig('rf_hfe_joint',[-1.4])
+fullBody.client.basic.robot.setJointConfig('rh_hfe_joint',[-1.4])
+
 q_init = fullBody.getCurrentConfig(); q_init[0:7] = tp.q_init[0:7]
 q_goal = fullBody.getCurrentConfig(); q_goal[0:7] = tp.q_goal[0:7]
+
 
 # Randomly generating a contact configuration at q_init
 fullBody.setCurrentConfig (q_init)
@@ -94,10 +100,11 @@ r(q_init)
 configs = fullBody.interpolate(0.2, 1, 0)
 # RB: Is this line redundent?
 r.loadObstacleModel ('hpp-rbprm-corba', name_of_scene, "contact")
+r.client.gui.setColor('contact', [1,1,1,0.3])
 
 i = 0;
-# fullBody.draw(configs[i],r); i=i+1; i-1
-
+#~ # fullBody.draw(configs[i],r); i=i+1; i-1
+#~ 
 while (i < len(configs)):
 	fullBody.draw(configs[i],r)
 	sleep(0.05)
